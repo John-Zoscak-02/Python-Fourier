02a7d8fe2e0acecc214da40b92c73a217d4620a5
John-Zoscak-02 <jzoscak02@gmail.com>
2020-12-20 21:07:39 -0500

    Vector Class Start

--- Fourier-Series/Vector.py
+++ Fourier-Series/Vector.py
@@ -1,6 +1,22 @@
 class Vector:
 
-
-
     def __init__(self):
-    
+        self.real = 0
+        self.imaginary = 0
+        self.complex = 0 + 0j
+
+    def setComponents(complex):
+        self.real = complex.real
+        self.imaginary = complex.imag
+        self.complex = complex.conjugate()
+
+    def resetComponents(self):
+        self.real = 0
+        self.imaginary = 0
+        self.complex = 0 + 0j
+
+    def print(self):
+        print("Real Component: %d" % self.real)
+        print("Imaginary Component: %d" % self.imaginary)
+        print("Real Component: %d" % self.complex)
+
