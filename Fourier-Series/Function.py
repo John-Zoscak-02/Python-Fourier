f66bcc214798a03e4ee0c6227a7e137dec96002f
John-Zoscak-02 <jzoscak02@gmail.com>
2020-12-22 12:07:16 -0500

    Reorganized program structure and added framework for a few vital components

--- Fourier-Series/Function.py
+++ Fourier-Series/Function.py
@@ -1,9 +1,34 @@
+from Vector import Vector
+
+
 class Function:
 
     def __init__(self, coef, n):
         self.coef = coef
         self.n = n
+        self.vector = Vector()
 
     def setFunction(self, coef, n):
         self.coef = coef
         self.n = n
+
+    def print(self):
+        print("%de^(%d*2(pi)it)" % (self.coef, self.n))
+
+class Calculator:
+
+    def __init__(self, vector):
+        self.vector = vector
+
+    @staticmethod
+    def calculateConstant(n):
+        import math
+        import scipy.integrate as sci
+        import numpy as np
+        return sci.quad(lambda t: math.e ** (-n * 2 * math.pi * 1j * t), 0, 1)
+
+    @staticmethod
+    def calculateVector(function, time):
+        #calculate a vector given a function and a time
+        return Vector()
+
--- Fourier-Series/Main.py
+++ Fourier-Series/Main.py
@@ -1,12 +1,18 @@
+from Function import Calculator
+
 time = 0
+functions = []
 
 def main():
     global time
-    # do stuff
+    for function in functions :
+        v = Calculator.calculateVector(function, time)
+        #send v to GUI
     time += 1
 
 
 if __name__ == "__main__":
-    #initialize vectors
+    global functions
+    # initialize all the functions
     while True:
         main()
